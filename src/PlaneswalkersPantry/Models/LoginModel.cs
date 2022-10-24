6f3e480b8b3b2c2230ebacd39fb2faec8f2b27ae
William Kent <wjk011@gmail.com>
2022-10-24 14:08:26 -0400

    Set display name

--- src/PlaneswalkersPantry/Models/LoginModel.cs
+++ src/PlaneswalkersPantry/Models/LoginModel.cs
@@ -4,9 +4,10 @@ namespace PlaneswalkersPantry.Models;
 
 public class LoginModel
 {
-    [Required]
+    [Display(Name = "User Name:"), Required]
     public string? UserName { get; set; }
-    [Required]
+
+    [Display(Name = "Password:"), Required]
     public string? Password { get; set; }
 
     public bool IsInvalid { get; set; } = false;
