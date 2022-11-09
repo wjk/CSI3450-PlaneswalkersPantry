5d926a72506ec841e162fa174367241504163e6e
William Kent <wjk011@gmail.com>
2022-11-09 14:03:03 -0500

    Add DomainDMU deck insertion script

---
+++ src/PlaneswalkersPantry/SqlScripts/DomainDMU.sql
@@ -0,0 +1,4 @@
+INSERT INTO DECK (DECK_NUMBER, DECK_TITLE, DECK_COLORS) VALUES (10, 'DomainDMU', 'WUBRG');
+
+INSERT INTO CARD (CARD_NUMBER, CARD_TITLE, MANA_COST, RULES_TEXT, FLAVOR_TEXT, CARD_IMAGE, SET_CODE, RARITY_CODE, POWER, TOUGHNESS, TYPE_LINE, NUMBER_OWNED) VALUES (10001, 'Llanowar Greenwidow', '2G', 'Reach, trample.
+Domain — 7G: Return Llanowar Greenwidow from your graveyard to the battlefield tapped. It gains "If this permanent would leave the battlefield, exile it instead of putting it anywhere else." This ability costs 1 less to activate for each basic land type among lands you control.', NULL, NULL, 'DMU', 'R', 4, 3, 'Creature - Spider', 17);
